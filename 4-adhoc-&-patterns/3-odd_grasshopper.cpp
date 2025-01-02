b18b3892fefeb68bfe54ad22ed3d2456a49ada1b
Sandipan Mahata <Sandipan.m@muvin.in>
2025-01-02 18:25:42 +0530

    done

---
+++ 4-adhoc-&-patterns/3-odd_grasshopper.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler.
+
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i) // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interver from l to r r inclusive
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i) // reverse 1 based
+
+// short hand for usual tokens
+#define pb push_back
+#define fi first
+#define se second
+
+// to be used with algorithms that processes a container Eg: find(all(c),42)
+#define all(x) (x).begin(), (x).end() // Forward traversal
+#define rall(x) (x).rbegin, (x).rend() // Reverse traversal
+
+// traversal function to avoid long template definition. Now with C++11 auto alleviates the pain.
+#define tr(c,i) for(__typeof__((c)).begin() i = (c).begin(); i != (c).end(); i++)
+
+// find if a given value is present in a container. Container version. Runs in log(n) for set and map
+#define present(c,x) ((c).find(x) != (c).end())
+
+// find version works for all containers. This is present in std namespace.
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+
+// Avoiding wrap around of size()-1 where size is a unsigned int.
+#define sz(a) int((a).size())
+
+
+using namespace std;
+
+// Shorthand for commonly used types
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+
+void solve() {
+    ll t;
+    cin>>t;
+
+    while (t--)
+    {
+        /* code */
+    
+
+    ll x, n;
+    cin>>x>>n;
+
+    ll mod = n % 4;
+    ll final_pos;
+
+    if(mod == 1) 
+        final_pos = -n;
+    else if(mod == 2)
+        final_pos = 1;
+    else if(mod == 3)
+        final_pos = n + 1;
+    else
+        final_pos = 0;
+
+    if(x & 1)
+        final_pos = x - final_pos;
+    else final_pos = x + final_pos;
+
+    cout<<final_pos<<endl;
+    
+    }
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+    cout << fixed;
+    solve();
+    return 0;
+}
